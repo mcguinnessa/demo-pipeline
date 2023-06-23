df58b1b6835d77db3511423c6177007b9c15c011
Ubuntu <ubuntu@ip-172-31-4-8.eu-west-2.compute.internal>
2023-06-23 10:10:09 +0000

    add auto installers

---
+++ docker-install.sh
@@ -0,0 +1,17 @@
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+
+
+curl 'http://localhost:8080/api/v1/cli?arch=amd64&platform=linux' -o fly     && sudo chmod +x ./fly && sudo mv ./fly /usr/local/bin/
