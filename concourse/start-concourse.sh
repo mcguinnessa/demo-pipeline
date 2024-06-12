cd26c36978977be757c90abb73c1735abc39b894
Ubuntu <ubuntu@ip-172-31-5-13.eu-west-2.compute.internal>
2024-06-12 15:25:12 +0000

    Add in stop concourse and helper lines

--- concourse/start-concourse.sh
+++ concourse/start-concourse.sh
@@ -1,1 +1,4 @@
 EXTERNAL_IP=`dig +short myip.opendns.com @resolver1.opendns.com` docker-compose up -d
+
+echo Open: http:`dig +short myip.opendns.com @resolver1.opendns.com`:8080
+
---
+++ concourse/stop-concourse.sh
@@ -0,0 +1,2 @@
+EXTERNAL_IP=`dig +short myip.opendns.com @resolver1.opendns.com` docker-compose down
+
