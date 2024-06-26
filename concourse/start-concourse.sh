d6733a6d14bb3bbd72ed89a4214db4897ef76f15
Ubuntu <ubuntu@ip-172-31-14-111.eu-west-2.compute.internal>
2024-06-26 14:27:40 +0000

    add in fly command

--- concourse/start-concourse.sh
+++ concourse/start-concourse.sh
@@ -2,3 +2,5 @@ EXTERNAL_IP=`dig +short myip.opendns.com @resolver1.opendns.com` docker-compose 
 
 echo Open: http:`dig +short myip.opendns.com @resolver1.opendns.com`:8080
 
+echo "curl 'http://localhost:8080/api/v1/cli?arch=amd64&platform=linux' -o fly && sudo chmod +x ./fly && sudo mv ./fly /usr/local/bin/"
+
